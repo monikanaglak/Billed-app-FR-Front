87920558b37fec307ea152a49684458027314957
Monika.N <63499824+monikanaglak@users.noreply.github.com>
2022-11-27 11:09:41 +0100

    "fixing bug 4 by changing the function"

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -86,24 +86,24 @@ export default class {
  
   handleEditTicket(e, bill, bills) {
 
-    if (this.counter === undefined || this.id !== bill.id)
+    /*if (this.counter === undefined || this.id !== bill.id)
     this.counter = 0
     
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     
     
-    if (this.counter % 2 === 0) {
+    if (this.counter % 2 === 0) {*/
       bills.forEach(b => {
       
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
       
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
-      //show bill on the left side
+      
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      this.counter++
-    } else {
+      /*this.counter++*/
+   /* } else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
       $('.dashboard-right-container div').html(`
@@ -111,7 +111,7 @@ export default class {
       `)
       $('.vertical-navbar').css({ height: '120vh' })
       this.counter ++
-    }
+    }*/
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
@@ -153,7 +153,7 @@ export default class {
     }
     
     bills.forEach(bill => {
-      $(`#status-bills-container${this.index} #open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
   
     return bills
